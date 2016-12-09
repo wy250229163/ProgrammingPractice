6b76d5e1b6626bed65947cdecd6cc8abe855e871
weiyang <m201570477@hust.edu.cn>
2016-12-09 12:51:56 +0800

    dijistra

---
+++ 09图论算法/Dijkstra算法_带权路径长度最短.cpp
@@ -0,0 +1,129 @@
+#include<cstdio>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+#define MAX_VERTEX_NUM 100
+#define Vertextype int
+#define Infinity 0xffff
+#define NotAVertex -1
+
+typedef struct EdgeNode
+{
+	int adjVertex;
+	int weight; //邻接权重 
+	EdgeNode *nextEdgeNode;
+}EdgeNode;
+
+typedef struct VerNode
+{
+	int Known;
+	Vertextype data;
+	int dist;
+	int path;
+	EdgeNode *firstedge;
+}VerNode;
+
+typedef struct Graph
+{
+	VerNode verNode[MAX_VERTEX_NUM];
+	int vertex_num,edge_num;
+}Graph;
+
+void CreateDAG(Graph &G,int n,int e)
+{
+	int i,j,w,k;
+	G.vertex_num=n;
+	G.edge_num=e;
+	for(i=1;i<=n;i++)
+	{
+		cin>>G.verNode[i].data;
+		G.verNode[i].Known=0;
+		G.verNode[i].dist=Infinity;
+		G.verNode[i].path=-1;
+		G.verNode[i].firstedge=NULL;
+	}
+	for(k=1;k<=e;k++)
+	{
+		EdgeNode *p;
+		p=new EdgeNode;
+		cin>>i>>j>>w;
+		p->adjVertex=j;
+		p->weight=w;
+		p->nextEdgeNode=G.verNode[i].firstedge;
+		G.verNode[i].firstedge=p;
+	}
+}
+
+void Dijkstra(Graph &G,int n,int start) //带权路径最短Dijkstra算法 
+{
+	VerNode V;
+    EdgeNode *w;
+    int i,j,temp_num,temp_dist;
+    G.verNode[start].dist=0;
+    
+ 	for(i=1;i<=n;++i) //n个点待查找 
+ 	{
+ 		temp_dist=Infinity;
+	 	temp_num=0;
+	    for(j=1;j<=n;++j) //先寻找未知的最短顶点
+	    {
+	    	if(G.verNode[j].Known==0 && G.verNode[j].dist<temp_dist)
+    		{
+		    	temp_dist=G.verNode[j].dist;
+		    	temp_num=j;
+		    }
+	    }
+	    G.verNode[temp_num].Known=1;
+	    w=G.verNode[temp_num].firstedge;
+	    while(w)
+	    {
+    		if(G.verNode[w->adjVertex].Known==0)
+    		{
+		    	if(G.verNode[temp_num].dist+w->weight < G.verNode[w->adjVertex].dist)
+		    	{
+		    		G.verNode[w->adjVertex].dist = G.verNode[temp_num].dist+w->weight;
+	    			G.verNode[w->adjVertex].path=G.verNode[temp_num].data;	    			
+	    		}
+		    }
+    		w=w->nextEdgeNode;
+    	}
+	}
+}
+
+int PrintPath(Graph &G,int n,int end) //打印最短路径信息，并返回路径长度
+{
+	if(G.verNode[end].path!=NotAVertex)
+	{
+		PrintPath(G,n,G.verNode[end].path);
+		printf(" => ");
+	}
+	printf("%d",G.verNode[end].data);
+}
+
+int main()
+{
+    Graph G;
+    CreateDAG(G,7,12); //给出图的顶点数及边数量
+    Dijkstra(G,7,1); //给出顶点数以及起点编号
+    PrintPath(G,7,6); //给出顶点数以及终点编号 
+}
+
+/********************
+
+1 2 3 4 5 6 7
+1 2 2
+1 4 1
+2 4 3
+2 5 10
+3 1 4
+3 6 5
+4 3 2
+4 5 2
+4 6 8
+4 7 4
+5 7 6
+7 6 1
+
+*********************/ 
--- 09图论算法/无权路径算法.cpp
+++ 09图论算法/无权路径算法.cpp
@@ -10,82 +10,82 @@ using namespace std;
 
 typedef struct EdgeNode
 {
-	int adjVertex;
-	EdgeNode *nextEdgeNode;
+    int adjVertex;
+    EdgeNode *nextEdgeNode;
 }EdgeNode;
 
 typedef struct VerNode
 {
-	Vertextype data;
-	int dist;
-	Vertextype path;
-	EdgeNode *firstedge;
+    Vertextype data;
+    int dist;
+    Vertextype path;
+    EdgeNode *firstedge;
 }VerNode;
 
 typedef struct Graph
 {
-	VerNode verNode[MAX_VERTEX_NUM];
-	int vertex_num,edge_num;
+    VerNode verNode[MAX_VERTEX_NUM];
+    int vertex_num,edge_num;
 }Graph;
 
 void CreateDAG(Graph &G,int n,int e)
 {
-	int i,j,k;
-	G.vertex_num=n;
-	G.edge_num=e;
-	for(i=1;i<=n;i++)
-	{
-		cin>>G.verNode[i].data;
-		G.verNode[i].dist=Infinity;
-		G.verNode[i].path=0;
-		G.verNode[i].firstedge=NULL;
-	}
-	for(k=1;k<=e;k++)
-	{
-		EdgeNode *p;
-		p=new EdgeNode;
-		cin>>i>>j;
-		p->adjVertex=j;
-		p->nextEdgeNode=G.verNode[i].firstedge;
-		G.verNode[i].firstedge=p;
-	}
+    int i,j,k;
+    G.vertex_num=n;
+    G.edge_num=e;
+    for(i=1;i<=n;i++)
+    {
+        cin>>G.verNode[i].data;
+        G.verNode[i].dist=Infinity;
+        G.verNode[i].path=0;
+        G.verNode[i].firstedge=NULL;
+    }
+    for(k=1;k<=e;k++)
+    {
+        EdgeNode *p;
+        p=new EdgeNode;
+        cin>>i>>j;
+        p->adjVertex=j;
+        p->nextEdgeNode=G.verNode[i].firstedge;
+        G.verNode[i].firstedge=p;
+    }
 }
 
 void Unweighted(Graph &G,int n)
 {
-	queue<VerNode> Q;
-	VerNode V;
-	EdgeNode *w;
-	G.verNode[n].dist=0;
-	Q.push(G.verNode[n]);
-	while(!Q.empty())
-	{
-		V=Q.front();Q.pop();
-		w=V.firstedge;
-		while(w)
-		{
-			if(G.verNode[w->adjVertex].dist==Infinity)
-			{			
-				G.verNode[w->adjVertex].dist=V.dist+1;
-				G.verNode[w->adjVertex].path=V.data;
-				Q.push(G.verNode[w->adjVertex]);
-			}
-			w=w->nextEdgeNode;
-		}
-	}
+    queue<VerNode> Q;
+    VerNode V;
+    EdgeNode *w;
+    G.verNode[n].dist=0;  //起点处路径长度置为0
+    Q.push(G.verNode[n]);
+    while(!Q.empty())
+    {
+        V=Q.front();Q.pop();
+        w=V.firstedge;
+        while(w)
+        {
+            if(G.verNode[w->adjVertex].dist==Infinity)
+            {           
+                G.verNode[w->adjVertex].dist=V.dist+1;
+                G.verNode[w->adjVertex].path=V.data;
+                Q.push(G.verNode[w->adjVertex]);
+            }
+            w=w->nextEdgeNode;
+        }
+    }
 } 
 
 int main()
 {
-	int i;
-	Graph G;
-	CreateDAG(G,7,12);
-	Unweighted(G,3);
-	for(i=1;i<=7;i++)
-		printf("%d ",G.verNode[i].path);
+    int i;
+    Graph G;
+    CreateDAG(G,7,12); //给出图的顶点数及边数量
+    Unweighted(G,3); //指出起点
+    for(i=1;i<=7;i++)
+        printf("%d ",G.verNode[i].path); //打印路径
 }
 
-/*****
+/*****测试数据 
 1 2 3 4 5 6 7
 3 1
 3 6
