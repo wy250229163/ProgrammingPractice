4ed090298f66fab88cfe0bc455d3f7ce4de8719c
weiyang <m201570477@hust.edu.cn>
2016-12-20 16:25:51 +0800

    updata

--- 09图论算法/无向图割点算法.cpp
+++ 09图论算法/无向图割点算法.cpp
@@ -8,18 +8,20 @@ using namespace std;
 #define MAX_VERTEX_NUM 100
 
 int Counter;
-int Visit[MAX_VERTEX_NUM];
+int Visit[MAX_VERTEX_NUM]; //标记是否被访问过 
 int Num[MAX_VERTEX_NUM];
-int Low[MAX_VERTEX_NUM];
-char Parent[MAX_VERTEX_NUM];
-
+int Low[MAX_VERTEX_NUM]; //记录从当前顶点通过树边跟一条背向边能达到的最低顶点编号 
+int Parent[MAX_VERTEX_NUM];
+int isArt[MAX_VERTEX_NUM]; //用于标记是否已经被输出为割点了 
+ 
 void Init_Globalpara(void)
 {
 	Counter=1;
 	memset(Visit,0,sizeof(Visit));
 	memset(Num,0,sizeof(Num));
 	memset(Low,0,sizeof(Low));
-	memset(Parent,'A',sizeof(Parent));
+	memset(Parent,0,sizeof(Parent));
+	memset(isArt,0,sizeof(isArt));
 }
 
 typedef struct EdgeNode
@@ -70,27 +72,39 @@ void CreateDAG(Graph &G,int n,int e)
 	}
 }
 
-void FindArt(Graph &G,char Vert)
+void FindArt(Graph &G,char Vert) //一次DFS搞定，复杂度O(V+E)，前序遍历后序遍历混合使用 
 {
+	int children=0;
 	int Vnum=(Vert-'A'+1);
 	EdgeNode *w;
 	w=G.verNode[Vnum].firstedge;
 	Visit[Vnum]=1;
-	Low[Vnum]=Num[Vnum]=Counter++;
+	
+	Low[Vnum]=Num[Vnum]=Counter++; //CASE1 
 	
 	while(w)
 	{
 		int Wnum=w->adjVertex;
 		if(!Visit[Wnum])
 		{
-			Parent[Wnum]=Vert;
-			FindArt(G,Wnum-1+'A');
-			if(Low[Wnum]>=Num[Vnum])
-				cout<<char(Vnum-1+'A')<<" is an articulation point"<<endl;
-			Low[Vnum]=min(Low[Vnum],Low[Wnum]);
+			children++;
+			Parent[Wnum]=(Vert-'A'+1);
+			FindArt(G,char(Wnum-1+'A'));
+			//考虑根是否为割点，当且仅当深度优先树的根有不止一个儿子时，根才为节点 
+			if(Parent[Vnum]!=0 && Low[Wnum]>=Num[Vnum] && !isArt[Vnum])
+			{
+				isArt[Vnum]=1;
+				cout<<char(Vnum-1+'A')<<" is an articulation point."<<endl;
+			}	
+			else if(Parent[Vnum]==0 && children>1)
+			{
+				isArt[Vnum]=1;
+				cout<<char(Vnum-1+'A')<<" is an articulation point."<<endl;
+			}	
+			Low[Vnum]=min(Low[Vnum],Low[Wnum]); //CASE3
 		}
-		else if(Parent[Vnum]!=(Wnum-1+'A'))
-			Low[Vnum]=min(Low[Vnum],Num[Wnum]);
+		else if(Parent[Vnum]!=char(Wnum-1+'A'))
+			Low[Vnum]=min(Low[Vnum],Num[Wnum]); //CASE2
 		w=w->nextEdgeNode;
 	}
 }
@@ -107,10 +121,11 @@ int main()
 A B C D E F G
 A B
 B C
-C D
-D A
 C G
+C D
 D E
 E F
 F D
+D A
+输出：D C 
 **************/
